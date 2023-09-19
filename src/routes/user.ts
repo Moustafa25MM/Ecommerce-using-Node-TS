9f411824852e03e71a01e3ecac3603030a551d44
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-09-19 11:11:32 +0300

    user with authenication

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import { userMiddlewares } from '../middlewares/user';
 import { loginMethods } from '../middlewares/login';
+import { authMethods } from '../middlewares/auth';
 
 const router = Router();
 
 router.use('/login', loginMethods.userLogin);
 router.post('/register', userMiddlewares.createUser);
-router.patch('/update/:id', userMiddlewares.updateUser);
-router.get('/all', userMiddlewares.getAllUsers);
+router.patch(
+  '/update/:id',
+  authMethods.isAuthorized,
+  userMiddlewares.updateUser
+);
+router.get('/all', authMethods.isAuthorized, userMiddlewares.getAllUsers);
 export const userRoutes: Router = router;
