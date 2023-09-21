4e2c57f96b9f6838f6c0673f0a2ba4f6bd4383bb
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-09-21 19:03:10 +0300

    Product Variation model

---
+++ src/models/productVariation.ts
@@ -0,0 +1,41 @@
+import mongoose, { Document, Schema, Types } from 'mongoose';
+import Product from './product';
+
+// Define your enum
+export enum Size {
+  SMALL = 'S',
+  MEDIUM = 'M',
+  LARGE = 'L',
+  X_LARGE = 'XL',
+  XX_LARGE = 'XXL',
+}
+
+interface PRODUCTVARIATION extends Document {
+  productId: Types.ObjectId;
+  color: string;
+  size: Size;
+  quantity: number;
+}
+
+const productVariationSchema = new Schema<PRODUCTVARIATION>(
+  {
+    productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
+    color: { type: String, required: true },
+    size: { type: String, enum: Object.values(Size), required: true },
+    quantity: { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform(doc, ret) {
+        delete ret.password;
+      },
+    },
+  }
+);
+
+const ProductVariation = mongoose.model(
+  'productVariation',
+  productVariationSchema
+);
+export default ProductVariation;
